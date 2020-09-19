b292c7c0cacb22bbf6a3f58de2d6ad451f31a91d
Mudassir Ali <mudassirali36@gmail.com>
2020-09-20 00:07:23 +0500

    build

---
+++ client/build/precache-manifest.d647a17c600f0c2ad5bbdfb9f47b2f9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56b8a36ac558a8fe1cb4d20bebdcf4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31817c7e864692566819",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "820d8c2301a94e90fd36",
+    "url": "/static/css/main.3e0237c6.chunk.css"
+  },
+  {
+    "revision": "31817c7e864692566819",
+    "url": "/static/js/2.58abc8b5.chunk.js"
+  },
+  {
+    "revision": "deea066462e3852fb7f610c3fa67aa20",
+    "url": "/static/js/2.58abc8b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820d8c2301a94e90fd36",
+    "url": "/static/js/main.36afa387.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
